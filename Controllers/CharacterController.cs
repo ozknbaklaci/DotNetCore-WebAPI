e314250ca7e7fd394305d295000431b1f5c196c3
Özkan Baklacı <ozkan.baklaci@demirorenteknoloji.com>
2020-10-02 23:07:58 +0300

    Delete a Character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -45,6 +45,19 @@ namespace DotNetCore_WebAPI.Controllers
             {
                 return NotFound(response);
             }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCharacter(int id)
+        {
+            ServiceResponse<List<GetCharacterDto>> response = await _characterService.DeleteCharacter(id);
+            if (response.Data == null)
+            {
+                return NotFound(response);
+            }
+
             return Ok(response);
         }
     }
