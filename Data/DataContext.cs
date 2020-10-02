33a9d6a3be7038cd0977e4c4c4c6f81621508e4a
Özkan Baklacı <ozkan.baklaci@demirorenteknoloji.com>
2020-10-02 23:31:06 +0300

    Implementing the DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DotNetCore_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore_WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
